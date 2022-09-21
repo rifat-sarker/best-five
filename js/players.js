f2d3ea46c2f4c51cd1556c9d0c16e6195ed75095
rifat-sarker <rifatswd@gmail.com>
2022-09-21 17:28:23 +0700

    header added

--- js/players.js
+++ js/players.js
@@ -2,36 +2,37 @@ function addToPlayer(name, button){
     
     const playerList = document.getElementById('player-list');
     const li = document.createElement('li');
+    li.classList.add('selected');
     li.innerText = `${name}`;
 
     const playerListLeng = document.querySelectorAll('.selected');
      
     if(playerListLeng.length < 5){
-        
         playerList.appendChild(li);
     }
     else{
         alert('You can not add more than 5 players');
         return;
     }
-    
+
     button.setAttribute('disabled', true);
     
-    
 }
+
+
 function calculate(){
     const perPlayer = document.getElementById('per-player').value;
     const playerListLeng = document.querySelectorAll('.selected');
     const total = document.getElementById('total')
     total.innerText = `${parseFloat(perPlayer) * playerListLeng.length}`;
-    
+    console.log(perPlayer)
 
 }
 
 function calculateTotal(){
-    const managerCharge = document.getElementById('manager-charge').value;
-    const coachCharge = document.getElementById('coach-charge').value;
-    const total = document.getElementById('total').innerText;
+    const managerCharge = document.getElementById('manager-charge').value || 0;
+    const coachCharge = document.getElementById('coach-charge').value || 0;
+    const total = document.getElementById('total').innerText || 0;
     const subTotal = document.getElementById('subtotal');
     subTotal.innerText = `${parseFloat(total) + parseFloat(managerCharge) + parseFloat(coachCharge) }`;
 
